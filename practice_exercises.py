fdfcee9350921e435c0a2d02382287c7d263c2df
Faiza-AS <89973507+Faiza-AS@users.noreply.github.com>
2021-11-26 01:09:55 +0500

    Create practice_exercises.py

---
+++ practice_exercises.py
@@ -0,0 +1,142 @@
+#NOTE: Try using f-strings wherever you can 
+
+  
+  
+# Exercise 1:
+# Try displaying the following dictionary using the three formatting methods (%, str.format() and f-string)
+# So the output would be something like this: Hi I am Tim and I am a gardener.
+ name="Tim"
+ occupation="gardener"
+
+
+
+
+
+
+
+
+
+# Exercise 2:
+# Write a python program to check whether a given integer is a multiple of 5 or not
+# hint: Use the %(modulo operator) and if else statements.
+
+
+
+
+
+
+
+
+
+
+# Exercise 3
+# Input two numbers and write a program to check which one of the two is the largest. 
+# For example: 24 and 45---> 45 is the larger number
+# hint: Use if else statements and 'input' to get values from the user.
+
+
+
+
+
+
+
+
+
+# Exercise 4
+# Write a program to calculate the average of a set of given integers.
+# For example: if integer=5 then 1+2+3+4+5/5 would output 3.0, remember to add numbers exclusive of 0 and inclusive of the integer value
+# hint: use loops to calculate the sum of the numbers and use the formula avergae=sum_of_numbers/total_numbers
+
+
+
+
+
+
+
+
+
+
+# Exercise 5
+# Can you draw the following pattern.
+#     *
+#     * *
+#     * * *
+#     * * * *
+#     * * * * *
+# hint: use nested loops
+
+
+
+
+
+
+
+
+
+# Exercise 6
+# Can you make the same pattern in reverse
+#     * * * * * 
+#     * * * * 
+#     * * * 
+#     * * 
+#     * 
+  
+
+
+
+
+
+
+
+
+
+# Exercise 7
+# understand indexing
+# tuple1[0] = 'Orange'
+# tuple1[1] = [10, 20, 30]
+# tuple1[2]=  [5, 15, 25]
+
+#Access the value 20 from the tuple
+
+tuple1 = ("Orange", [10, 20, 30], (5, 15, 25))
+
+
+
+
+
+
+# Exercise 8
+# Print characters placed at even index numbers from the string of users choice
+# For example: word= REDI, index 0= R, index 2= D
+# hint: use for loop and play with the step size of the range
+
+
+
+
+
+
+
+
+# Exercise 9
+# Swap the values of two variables x=5 and y=10
+# So the output after the swapping should be x=10 and y=5
+# hint: use another variable to store the value
+ 
+
+
+
+
+
+
+
+# Exercise 10
+# How many times does the letter 'b' appear in the string below
+# hint: use the built in count() method to calculate the number of times a specified element appears.
+ numbers = "Betty bought a butter but the butter was bitter so Betty bought a butter to make the bitter butter better"
+
+  
+  
+  
+  
+  
+  
